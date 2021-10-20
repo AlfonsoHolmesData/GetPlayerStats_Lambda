988fefe9de7c26212b176037e6fe963ade7e414c
AlfonsoHolmesData <alholmes@Email.peace.edu>
2021-10-20 15:40:06 -0400

    fixed pkg structure
    Signed-off-by: AlfonsoHolmesData <alholmes@Email.peace.edu>

--- src/main/java/com/revature/get_player_stats/GetPlayerStatsHandler.java
+++ src/main/java/com/revature/get_player_stats/GetPlayerStatsHandler.java
@@ -7,6 +7,8 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.revature.get_player_stats.Documents.User;
+import com.revature.get_player_stats.Repository.UserRepository;
 
 import java.util.List;
 
